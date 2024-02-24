31792f52827d9456da345a2e4bdbf1695f9953ef
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-02-24 14:04:20 -0600

    Added formal links don't work

---
+++ script.js
@@ -0,0 +1,3 @@
+function cow() {
+    Swal.fire({icon: "error",title: "Link not found",text: "Sorry the links don't work at this time. \n Please contact us at slimespritz@gmail.com \n Thanks!",});
+}
