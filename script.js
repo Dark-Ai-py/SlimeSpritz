69218ec64ccd4db8663d831661afa3fa9f6f7369
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-04-09 18:39:23 -0500

    removed speed insites

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-
 function cow() {
     Swal.fire({icon: "error",title: "Link not found",text: "Sorry the links don't work at this time. \n Please contact us at slimespritz@gmail.com \n Thanks!",});
 }
-injectSpeedInsights();
