870af5324c9c5da09681938f5dc4449a1d1b389e
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-04-09 18:37:08 -0500

    fixed popup

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { injectSpeedInsights } from '@vercel/speed-insights';
 
-injectSpeedInsights();
+
 function cow() {
     Swal.fire({icon: "error",title: "Link not found",text: "Sorry the links don't work at this time. \n Please contact us at slimespritz@gmail.com \n Thanks!",});
 }
+injectSpeedInsights();
