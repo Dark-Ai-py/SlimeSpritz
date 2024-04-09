d59ab7d1d8e5487fda0e3785525b8fadaae79750
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-04-09 18:34:18 -0500

    added vercel monitering

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 function cow() {
     Swal.fire({icon: "error",title: "Link not found",text: "Sorry the links don't work at this time. \n Please contact us at slimespritz@gmail.com \n Thanks!",});
 }
